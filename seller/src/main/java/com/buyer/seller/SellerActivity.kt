2b00e13c5e26c0bcd22947a2eb23757a6fa60c3e
gptshubham595 <gptshubham595@gmail.com>
2022-11-06 00:48:26 +0530

    initial commit

---
+++ seller/src/main/java/com/buyer/seller/SellerActivity.kt
@@ -0,0 +1,11 @@
+package com.buyer.seller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SellerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_seller)
+    }
+}
